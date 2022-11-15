172f154e3b42d91173dc0049664cb056f9ac51e8
Rodrigo Aniceto <rodrigo_anicetoo@yahoo.com.br>
2022-11-15 11:01:00 -0300

    coleta turma submissão arquivo

--- src/app.py
+++ src/app.py
@@ -37,7 +37,12 @@ def telaInicial():
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data # coleta do arquivo
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Salvar o arquivo
+        nome_turma = request.form['nometurma']
+        if nome_turma == "":
+            return render_template('index.html', listaturmas=lista_turmas,form=form, mensagem="Favor informar nome turma")
+        else:
+            file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Salvar o arquivo
+            return ("nome arquivos: "+ file.filename+" nome turma: "+request.form['nometurma'])
         return render_template('index.html', listaturmas=lista_turmas,form=form, mensagem="Arquivo submetido com sucesso")
 
     return render_template('index.html', listaturmas=lista_turmas,form=form, mensagem="")
